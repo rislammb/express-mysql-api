ffeefbc8dd2ee52be108db2a0881694aceeed901
Rayhanul Islam <rislammb@gmail.com>
2024-10-06 08:56:36 +0600

    remove db config for test api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use("/posts", postRouter);
 
-db.sequelize.sync().then(() => {
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => console.log(`Server listening on port ${port}`));
-});
+// db.sequelize.sync().then(() => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+// });
