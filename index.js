b2d76e9db5e720840560d84cb11396a09c1460b3
Rayhanul Islam <rislammb@gmail.com>
2024-10-06 19:12:19 +0600

    add force sync for models

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/posts", postRouter);
 
 db.sequelize
-  .sync()
+  .sync({ force: true })
   .then(() => {
     const port = process.env.PORT || 5000;
     app.listen(port, () => console.log(`Server listening on port ${port}`));
--- routes/posts.js
+++ routes/posts.js
@@ -4,10 +4,10 @@ const { Post } = require("../models");
 router.get("/", async (_req, res) => {
   try {
     const posts = await Post.findAll();
-    res.json([]);
+    return res.status(200).json(posts);
   } catch (error) {
     console.log("Error from get posts : ", error);
-    res.status(500).json(JSON.stringify(error));
+    return res.status(500).json(JSON.stringify(error));
   }
 });
 
@@ -15,10 +15,10 @@ router.post("/", async (req, res) => {
   const post = req.body;
   try {
     const data = await Post.create(post);
-    res.status(201).json(data);
+    return res.status(201).json(data);
   } catch (error) {
     console.log("Error from create post : ", error);
-    res.json(JSON.stringify(error));
+    return res.json(JSON.stringify(error));
   }
 });
 
