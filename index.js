05b557549cbde2c6deab5e43c18bc046875463c0
Rayhanul Islam <rislammb@gmail.com>
2024-10-06 09:18:55 +0600

    back index.js file to base

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const db = require("./models");
+const postRouter = require("./routes/posts.js");
+
 const app = express();
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.use(cors());
+app.use(express.json());
+
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Server listening on port ${port}`));
+});
