1bf72b0c500c6582528e8619c616e66ccd999145
Rayhanul Islam <rislammb@gmail.com>
2024-10-06 08:54:31 +0600

    try to fix production config issue

--- config/config.js
+++ config/config.js
@@ -8,12 +8,14 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
+    logging: false,
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: `${process.env.DB_NAME}_test`,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false,
   },
@@ -24,5 +26,6 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
+    logging: false,
   },
 };
