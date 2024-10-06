c012c9f5a4f761d551e7ca05c6205452a427e27d
Rayhanul Islam <rislammb@gmail.com>
2024-10-06 09:13:16 +0600

    clear comment lines from index file

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 const express = require("express");
-const cors = require("cors");
-// const db = require("./models");
-// const postRouter = require("./routes/posts.js");
-
 const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/posts", postRouter);
-
-// db.sequelize.sync().then(() => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-// });
