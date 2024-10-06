c0c7d2449ed0b7f328078b325f6e228eedb4e0ae
Rayhanul Islam <rislammb@gmail.com>
2024-10-06 09:36:31 +0600

    try to fix db connection issue

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
-// const cors = require("cors");
-// const db = require("./models");
-// const postRouter = require("./routes/posts.js");
+const cors = require("cors");
+const db = require("./models");
+const postRouter = require("./routes/posts.js");
 
 const app = express();
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-// app.use("/posts", postRouter);
+app.use("/posts", postRouter);
 
-// db.sequelize.sync().then(() => {
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
-// });
+db.sequelize
+  .sync()
+  .then(() => {
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+  })
+  .catch((err) => console.log("Faild to connect DB: ", err));
