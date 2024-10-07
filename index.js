fa78bc1f069faa007c6f016ddbccf7a34ebf46c4
Rayhanul Islam <rislammb@gmail.com>
2024-10-07 06:19:21 +0600

    another update

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/posts", postRouter);
+
 db.sequelize
-  .sync()
+  .sync({ force: false })
   .then(() => {
-    app.use("/posts", postRouter);
-    console.log("Database connected.");
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
   })
   .catch((err) => console.log("Faild to connect DB: ", err));
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
